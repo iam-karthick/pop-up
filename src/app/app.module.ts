0cf24c642a29b92a26a8bc72d5024c3bcf738388
javakarthicklog <javakarthiklog@gmail.com>
2017-12-04 08:44:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PopComponent } from './pop/pop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopComponent
   ],
   imports: [
     BrowserModule
